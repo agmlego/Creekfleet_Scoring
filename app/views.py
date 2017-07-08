eff5391e8c6204d8b18846ae0f9f786f67864b7e
Andrew G. Meyer <agmlego@gmail.com>
2017-07-08 15:00:32 -0400

    Edited sample information.

--- app/views.py
+++ app/views.py
@@ -5,7 +5,13 @@ from datetime import date,time,timedelta
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
+    user = {
+        'name': 'Andrew Meyer',
+        'email':'agmlego@gmail.com',
+        'admin':True,
+        'password':'b7245408f2f4ad5f7d45addef169b2c2508f7988ea69ca27d3239666a335748c',
+        'phone':'+1 906-569-1110'
+    }  # fake user
     events = [
         {
             'date':date(2017,07,06),
@@ -90,7 +96,7 @@ def index():
                                 'model':'Pintail',
                                 'length':14,
                                 'units':'feet',
-                                'portsmouthHC':105
+                                'portsmouthHC':105.0
                             },
                             'time':timedelta(minutes=21,seconds=38.99),
                             'handicap':0.98,
@@ -102,12 +108,12 @@ def index():
                             'place':2,
                             'skipper':'Marc Meyer',
                             'boat':{
-                                'sailNum':340,
+                                'sailNum':253,
                                 'make':'MFG',
                                 'model':'Pintail',
                                 'length':14,
                                 'units':'feet',
-                                'portsmouthHC':105
+                                'portsmouthHC':105.0
                             },
                             'time':timedelta(minutes=21,seconds=37.99),
                             'handicap':1.01,
@@ -115,8 +121,74 @@ def index():
                             'corrected':timedelta(seconds=(timedelta(minutes=21,seconds=37.99).total_seconds()*1.01)),
                             'shortCourse':False
                         },
+                        {
+                            'place':3,
+                            'skipper':'Dan Shock',
+                            'boat':{
+                                'sailNum':5266,
+                                'make':'Nickels',
+                                'model':'Buccaneer',
+                                'length':18,
+                                'units':'feet',
+                                'portsmouthHC':86.9
+                            },
+                            'time':timedelta(minutes=22,seconds=37.99),
+                            'handicap':1.05,
+                            'nextHandicap':1.05,
+                            'corrected':timedelta(seconds=(timedelta(minutes=22,seconds=37.99).total_seconds()*1.05)),
+                            'shortCourse':False
+                        }
                     ]
-
+                    'pmResults':[
+                        {
+                            'place':1,
+                            'skipper':'Marc Meyer',
+                            'boat':{
+                                'sailNum':340,
+                                'make':'MFG',
+                                'model':'Pintail',
+                                'length':14,
+                                'units':'feet',
+                                'portsmouthHC':105.0
+                            },
+                            'time':timedelta(minutes=21,seconds=37.99),
+                            'corrected':timedelta(seconds=(timedelta(minutes=21,seconds=37.99).total_seconds()*100/105.0)),
+                            'shortCourse':False
+                        },
+                        {
+                            'place':2,
+                            'skipper':'Andrew Meyer',
+                            'boat':{
+                                'sailNum':253,
+                                'make':'MFG',
+                                'model':'Pintail',
+                                'length':14,
+                                'units':'feet',
+                                'portsmouthHC':105.0
+                            },
+                            'time':timedelta(minutes=21,seconds=38.99),
+                            'corrected':timedelta(seconds=(timedelta(minutes=21,seconds=38.99).total_seconds()*100/105.0)),
+                            'shortCourse':False
+                        },
+                        {
+                            'place':3,
+                            'skipper':'Dan Shock',
+                            'boat':{
+                                'sailNum':5266,
+                                'make':'Nickels',
+                                'model':'Buccaneer',
+                                'length':18,
+                                'units':'feet',
+                                'portsmouthHC':86.9
+                            },
+                            'time':timedelta(minutes=22,seconds=37.99),
+                            'corrected':timedelta(seconds=(timedelta(minutes=22,seconds=37.99).total_seconds()*100/86.9)),
+                            'shortCourse':False
+                        }
+                    ]
+                },
+            ]
+        },
     ]
 
     return render_template('index.html',
